bf67ea6878edcedffe20ac67bcb77347c2731b7e
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-02-11 17:43:50 +0900

    [Update] WhisperManager

--- Speech-To-Text-Module/Assets/Scripts/Managers/WhisperManager.cs
+++ Speech-To-Text-Module/Assets/Scripts/Managers/WhisperManager.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using OpenAI;
+using System;
+using NUnit.Framework;
+using System.IO;
 
 public class WhisperManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private OpenAIApi openAI = new OpenAIApi();
 
-    // Update is called once per frame
-    void Update()
+    public async void AskWhisper()
     {
-        
+        string filePath = Environment.CurrentDirectory + "/Assets/Datas/hi.m4a";
+
+        if (!File.Exists(filePath))
+        {
+            Debug.LogError("File not Exist: " + filePath);
+            return;
+        }
+
+        var req = new CreateAudioTranscriptionsRequest
+        {
+            File = filePath,
+            Model = "whisper-1",
+            Language = "ko"
+        };
+
+        var res = await openAI.CreateAudioTranscription(req);
+        Assert.NotNull(res);
+
+        Debug.Log(res.Text);
     }
 }
