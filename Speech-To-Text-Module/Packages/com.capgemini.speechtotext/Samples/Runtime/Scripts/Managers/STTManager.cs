f5772a4e840f1cbbbc19f60569e417599c56067d
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-02-19 13:56:31 +0900

    [Update] Package.json file

---
+++ Speech-To-Text-Module/Packages/com.capgemini.speechtotext/Samples/Runtime/Scripts/Managers/STTManager.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+public class STTManager : Singleton<STTManager>
+{
+    [field: Header("File Info")]
+    [field: SerializeField] public string FilePath { get; private set; }
+    [field: Header("Converted Text")]
+    [field: SerializeField] public string ConvertedText { get; private set; }
+    [field: Header("Selected Model")]
+    [field: SerializeField] public ESTTType STTModel { get; private set; }
+    [field: Header("Set Audio Maximum Length")]
+    [field: SerializeField, Range(10f, 30)] public int MaximumAudioLength = 20;
+
+    [field: SerializeField] public bool IsTranscribe = false;
+
+    public void Initialize()
+    {
+        SetConvertedText("");
+        SetFilePath("");
+    }
+
+    public void SetModelType(ESTTType sttModel)
+    {
+        STTModel = sttModel;
+    }
+
+    public void SetFilePath(string filePath)
+    {
+        FilePath = filePath;
+    }
+
+    public void SetConvertedText(string text)
+    {
+        ConvertedText = text;        
+    }
+
+    public void SetTranscribeStatus(bool isTranscribe)
+    {
+        IsTranscribe = isTranscribe;
+    }
+
+    public bool IsTranscribing()
+    {
+        return IsTranscribe;
+    }
+
+}
---
+++ Speech-To-Text-Module/Packages/com.capgemini.speechtotext/Samples/Runtime/Scripts/Managers/WhisperManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using OpenAI;
+using System.IO;
+using NUnit.Framework;
+using Cysharp.Threading.Tasks;
+
+public class WhisperManager : Singleton<WhisperManager>
+{
+    [Header("Output")]
+    [SerializeField] private string outputString = "";
+
+    private OpenAIApi openAI = new OpenAIApi();
+
+    [ContextMenu ("AskWhisper")]
+    public async UniTask<string> AskWhisper(AudioClip audioClip)
+    {
+        if (STTManager.Instance.IsTranscribing()) return null; // avoid duplicate execution 
+        STTManager.Instance.SetTranscribeStatus(true);
+
+        string filePath = Path.Combine(Application.dataPath, "AudioProcessings", audioClip.name);
+        var req = new CreateAudioTranscriptionsRequest
+        {
+            File = filePath,
+            Model = "whisper-1", 
+            Language = "en", // target language
+        };
+
+        var res = await openAI.CreateAudioTranscription(req);
+
+        STTManager.Instance.SetTranscribeStatus(false);
+        Assert.NotNull(res); //response null check
+        
+        Debug.Log(res);
+        outputString = res.Text;
+
+        ExtensionMethods.RemoveProcessedAudioFile();
+        STTManager.Instance.SetConvertedText(outputString);
+        return outputString;
+    }
+}
